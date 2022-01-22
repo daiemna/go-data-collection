c57b78b592d58b93485387d1a91e05660a9f5012
Dali <daiem.dna@gmail.com>
2022-01-22 18:21:29 +0100

    refactoring and docs.

--- internal/bigdb/csvhelper.go
+++ internal/bigdb/csvhelper.go
@@ -2,57 +2,68 @@ package bigdb
 
 import (
 	"errors"
+	"fmt"
 	"strconv"
 
-	"github.com/gocql/gocql"
+	"github.com/rs/zerolog/log"
 )
 
-type csvLineParser interface {
+//CsvLineParser is interface to parse a csv line to database row.
+type CsvLineParser interface {
+	//fromLine must be implemented to receive tokenized csv line and parse it.
 	fromLine(line []string) error
+	//getRow must be implemented to retrive the parsed row.
+	getRow() *CsvRow
 }
 
-type csvRow struct {
+type CsvRow struct {
 	timestamp int64
 	datapoint string
 	value     float64
 }
 
-type csvRowWithUUID struct {
-	csvRow
-	datapoint gocql.UUID
+func (row *CsvRow) getRow() *CsvRow {
+	return row
 }
 
-func (row *csvRow) fromLine(line []string) error {
+func (row *CsvRow) fromLine(line []string) error {
 	utcTime, err := convertToUTCMilli(line[0])
-	ts, a2iErr := strconv.ParseInt(line[0], 10, 64)
-	if err != nil && a2iErr != nil {
-		return errors.New("Failed to parse the timestamp: " + err.Error())
+	ts, toIntErr := strconv.ParseInt(line[0], 10, 64)
+	if err != nil && toIntErr != nil {
+		return fmt.Errorf("failed to parse the timestamp `%v`: %v", line[0], err.Error())
 	}
 	if err == nil {
 		row.timestamp = utcTime
-	} else if a2iErr == nil {
+	} else if toIntErr == nil {
 		row.timestamp = ts
 	}
 
 	val, err := toFloat(line[2])
 	if err != nil {
-		return errors.New("Illegal value format")
+		return errors.New("illegal value format, value must be float")
 	}
 	row.datapoint = line[1]
 	row.value = val
 	return nil
 }
 
-func (row *csvRowWithUUID) fromLine(line []string) error {
-	err := row.csvRow.fromLine(line)
+//ParseCsvLine attempts to parse csv line tokens to database row,
+// which can be retrived by calling getRow().
+func ParseCsvLine(line []string) (CsvLineParser, error) {
+	csvLineUUID := &csvRowWithUUID{}
+	err := csvLineUUID.fromLine(line)
 	if err != nil {
-		return err
+		csvLine := &CsvRow{}
+		perr := csvLine.fromLine(line)
+		if perr != nil {
+			log.Debug().Msgf("attempt to parse id to UUID also failed: %v", err)
+			return nil, perr
+		}
+		return csvLine, nil
 	}
-	tsidUUIDFormat, err := gocql.ParseUUID(line[1])
+	err = csvLineUUID.CsvRow.fromLine(line)
 	if err != nil {
-		return errors.New("Failed to parse the UUID")
-
+		return nil, fmt.Errorf("error parsing line: %v", err)
 	}
-	row.datapoint = tsidUUIDFormat
-	return nil
+	return csvLineUUID, nil
 }
